afa48ca01f34a7d216e2c6e64e3bc4acfbc0afb1
gplorovg <gplorovg@gmail.com>
2023-08-09 01:42:08 +0300

    add vote link

--- bot.py
+++ bot.py
@@ -35,16 +35,17 @@ async def create_calendar(update: Update, context: ContextTypes.DEFAULT_TYPE):
     event_name = "test_name"
     date1 = "08.07.2023"
     date2 = "10.07.2023"
-    group_id = context.args[0][4:]
-    event_id = calc.get_event_id(group_id, event_name, date1, date2)
+    group_id = int(context.args[0][4:])
+    event_id = calc.get_event_id(str(group_id), event_name, date1, date2)
     picture_path = "pictures/" + str(event_id) + ".png"
     url = helpers.create_deep_linked_url(context.bot.username, "eid_" + str(event_id))
     keyboard = InlineKeyboardMarkup.from_button(
         InlineKeyboardButton(text="FINISH", url=url)
     )
     text = "Just Do IT"
-    await db.create("calendars", {"group_id": group_id, "event_id": event_id, "event_name": event_name,
-                    "active": True, "picture_path": picture_path})
+    db.create("calendars", {"group_id": group_id, "event_id": event_id, "event_name": event_name,
+              "active": True, "picture_path": picture_path})
+    db.commit()
     calendar = Calendar(date1, date2, event_name)
     calendar.save(picture_path)
     pic_message = await context.bot.send_photo(chat_id=group_id, photo=picture_path)
@@ -52,10 +53,15 @@ async def create_calendar(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=group_id, text=text, reply_markup=keyboard)
 
 
+async def vote(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print("Vote")
+
+
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TOKEN).build()
-
-    application.add_handler(CommandHandler("start", start))
+    # Make sure the deep-linking handlers occur *before* the normal /start handler.
     application.add_handler(CommandHandler("start", create_calendar, filters.Regex("gid_")))
+    application.add_handler(CommandHandler("start", vote, filters.Regex("eid_")))
+    application.add_handler(CommandHandler("start", start))
 
     application.run_polling()
