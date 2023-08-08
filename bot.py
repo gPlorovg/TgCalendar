34470a361ce0185d4d9462e4a6be51d4fd44a049
gplorovg <gplorovg@gmail.com>
2023-08-09 00:35:55 +0300

    add deep links

--- bot.py
+++ bot.py
@@ -1,33 +1,55 @@
 from os import getenv
 from dotenv import load_dotenv
 import logging
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram import Update, helpers, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, filters
 import calculate as calc
 from pics_generator import Calendar
+from db_manage import DataBase
 
 load_dotenv()
 TOKEN = getenv("TOKEN")
+USER = getenv("USER")
+HOST = getenv("HOST")
+DB_PASSWORD = getenv("DB_PASSWORD")
+db = DataBase("TgCalendar", USER, HOST, DB_PASSWORD)
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
+with open("manual") as f:
+    manual = f.read()
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="To create calendar text /create command"
-                                                                          " with edge dates of your event and it's name"
-                                                                          "\nLike this: /create 08.07-10.07"
-                                                                          " Author birthday party")
+    url = helpers.create_deep_linked_url(context.bot.username, "gid_" + str(update.effective_chat.id))
+    keyboard = InlineKeyboardMarkup.from_button(
+        InlineKeyboardButton(text="Push me!", url=url)
+    )
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=manual, reply_markup=keyboard)
 
 
 async def create_calendar(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    year = calc.get_year()
-    date1 = context.args[0].split("-")[0] + "." + str(year)
-    date2 = context.args[0].split("-")[1] + "." + str(year)
+    event_name = "test_name"
+    date1 = "08.07.2023"
+    date2 = "10.07.2023"
+    #При нажатии на кнопку create
+    picture_path = "pictures/" + db.event_counter + ".png"
+    db.create("calendars", {"group_id": context.args[0][4:], "event_id": db.event_counter, "event_name": event_name,
+                            "active": True, "picture_path": picture_path})
+    await generate_picture(update, context, date1, date2, picture_path)
+    url = helpers.create_deep_linked_url(context.bot.username, "eid_" + str(db.event_counter))
+    text = "Go and make your event!"
+    keyboard = InlineKeyboardMarkup.from_button(
+        InlineKeyboardButton(text="Push me!", url=url)
+    )
+    await context.bot.send_message(update.effective_chat.id, text, reply_markup=keyboard)
+
+
+async def generate_picture(update: Update, context: ContextTypes.DEFAULT_TYPE, date1, date2, path):
     calendar = Calendar(date1, date2, " ".join(context.args[1:]))
-    path = "calendar for" + " ".join(context.args[1:]) + ".png"
     calendar.save(path)
     pic_message = (await context.bot.send_photo(chat_id=update.effective_chat.id, photo=path))
     await context.bot.pin_chat_message(chat_id=update.effective_chat.id, message_id=pic_message.message_id)
@@ -36,9 +58,7 @@ async def create_calendar(update: Update, context: ContextTypes.DEFAULT_TYPE):
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TOKEN).build()
 
-    create_handler = CommandHandler("create", create_calendar)
-    start_handler = CommandHandler("start", start)
-    application.add_handler(start_handler)
-    application.add_handler(create_handler)
+    application.add_handler(CommandHandler("start", create_calendar, filters.Regex("gid_")))
+    application.add_handler(CommandHandler("start", start))
 
     application.run_polling()
