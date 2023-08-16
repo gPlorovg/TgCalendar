5c82d1b5c76f767786d032a51f5530d40bb69d05
gplorovg <gplorovg@gmail.com>
2023-08-16 23:24:59 +0300

    add create button

--- WebApp/static/config_calendar.js
+++ WebApp/static/config_calendar.js
@@ -1,11 +1,12 @@
-localStorage.setItem("event_name", "Roma's birthday party");
-localStorage.setItem("start_date", "2023-08-15");
-
-const event_label = document.createElement("label");
 const calendar = document.querySelector("table");
+const event_label = document.createElement("label");
+const create_btn = document.createElement("button");
+create_btn.innerText = "create calendar";
 event_label.textContent = localStorage.getItem("event_name");
 event_label.setAttribute("id", "event_name");
 calendar.insertAdjacentElement("beforebegin", event_label);
+calendar.insertAdjacentElement("afterend", create_btn);
+
 
 function draw_grid(positions, months_positions) {
     let dates_row;
@@ -43,6 +44,8 @@ function draw_grid(positions, months_positions) {
     }
 }
 
-fetch(window.location.origin + "/calendar_grid?start_date=" + localStorage.getItem("start_date"))
-    .then((json_data) => json_data.json())
-    .then((resolved_data) => draw_grid(resolved_data.positions, resolved_data.months_positions));
+draw_grid(['', '', '16.08.2023', '17.08.2023', '18.08.2023', '19.08.2023', '20.08.2023', '21.08.2023', '22.08.2023', '23.08.2023', '24.08.2023', '25.08.2023', '26.08.2023', '27.08.2023', '28.08.2023', '29.08.2023', '30.08.2023', '31.08.2023', '', '', '', '', '', '', '', '01.09.2023', '02.09.2023', '03.09.2023', '04.09.2023', '05.09.2023', '06.09.2023', '07.09.2023', '08.09.2023', '09.09.2023', '10.09.2023'],['August', '', '', 'September', '']);
+
+// fetch(window.location.origin + "/calendar_grid?start_date=" + localStorage.getItem("start_date"))
+//     .then((json_data) => json_data.json())
+//     .then((resolved_data) => draw_grid(resolved_data.positions, resolved_data.months_positions));
