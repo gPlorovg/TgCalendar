d1dcb773757fc73635e7398f41cdf0665d67839a
gplorovg <gplorovg@gmail.com>
2023-09-20 00:26:24 +0300

    add database connection and send data from db to site, modify vote function to get event_id

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template, request
-from calculate import get_today_date, generate_days, get_positions
+from os import getenv
+from dotenv import load_dotenv
 
+from flask import Flask, render_template, request, jsonify
+from calculate import get_today_date, generate_days, get_positions
+from db_manage import DataBase
 
 app = Flask(__name__)
 
+load_dotenv()
+HOST = getenv("HOST")
+USER = getenv("USER")
+DB_PASSWORD = getenv("DB_PASSWORD")
+db = DataBase("TgCalendar", USER, HOST, DB_PASSWORD)
 
 @app.get("/pre_config_calendar")
 def pre_config_calendar():
@@ -24,7 +32,18 @@ def calendar_grid():
 
 @app.get("/vote")
 def vote():
-    return render_template("vote.html")
+    event_id = request.args.get("e_id")
+    return render_template("vote.html", event_id=event_id)
+
+
+@app.get("/calendar_data")
+def calendar_data():
+    event_id = int(request.args.get("e_id"))
+    dates, event_name, mark_positions = db.read_calendar(event_id)
+    resp = get_positions(dates)
+    resp["mark_positions"] = mark_positions
+    resp["event_name"] = event_name
+    return jsonify(resp)
 
 
 if __name__ == '__main__':
